7be0d6ec7e54bdab071604b08f5220846a1a9bd4
Jose Jimenez <sharkyjj896@gmail.com>
2023-04-24 10:37:57 -0700

    Revert "yeetusTheAPIus"
    This reverts commit 9951aa32e399655d85af3ae4951878e14adebbce.

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const fs = require("fs")
 const bodyParser = require('body-parser')
 const forecastJson = require("./forecastdata.json")
 
+// Create an Express app
 var app = express()
-const port = process.env.PORT || 8081
+const port =  8000
 
+// Define color palette for the SVG recoloring
 var palette = [];
 palette.push(
 	"#3CC678",
@@ -21,32 +23,34 @@ palette.push(
 	"#F80F16"
 )
 
+// Configure Express app to use bodyParser middleware
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.json())
 
-app.get('', function (req, res) {
+// Define the route for fetching the main page JSON content
+app.get('/api', function (req, res) {
 	fs.readFile(__dirname + "/" + "main_page.json", 'utf8', function (err, data) {
 		if (err) {
 			console.log("Error reading file from disk: ", err)
 			return
 		}
 		try {
-			res.end(data)
+			res.json(JSON.parse(data))
 		} catch (err) {
 			console.log('Error parsing JSON string: ', err)
 		}
 	});
 })
 
-app.get('/parking/:time/:day', function (req, res) {
+// Define the route for fetching the recolored SVG based on the given time and day
+app.get('/api/:time/:day', function (req, res) {
 	fs.readFile(__dirname + "/" + "main_page.json", 'utf8', function (err, data) {
 		if (err) {
 			console.log("Error reading file from disk: ", err)
 			return
 		}
 		var page = JSON.parse(data)
-		var time = parseTime(req.params.time.replace(':', ''))
-		var svg = recolorSVG(time, parseDay(req.params.day))
+		var svg = recolorSVG(req.params.time.replace(':', ''), req.params.day)
 		page.content[6].content[0].groundOverlayImages = [
 			{
 				url: "data:image/svg+xml;base64,"+(new Buffer.from(svg).toString('base64')),
@@ -59,14 +63,22 @@ app.get('/parking/:time/:day', function (req, res) {
 			}
 		]
 		try {
-			res.end(JSON.stringify(page))
-		} catch (err) {
-			console.log('Error parsing JSON string: ', err)
+			
+			res.set({ 'content-type': 'application/json; charset=utf-8' });
+			res.json(page)
+		
+
+			
+		}
+		catch (err) {
+			console.log('Error parsing', err)
+
 		}
 	});
 })
 
-app.post('/date', function (req, res) {
+// Define the route for handling the date POST request
+app.post('/api/date', function (req, res) {
 	if (req.body) {
 		console.log("POST request for /date")
 
@@ -74,48 +86,22 @@ app.post('/date', function (req, res) {
 			"metadata": {
 				"version": "2.0",
 				"redirectLink": {
-					"relativePath": "/parking/" + req.body.time + "/" + req.body.day
+					"relativePath": '/' + req.body.time + "/" + req.body.day
 				}
 			}
 		}
 		res.end(JSON.stringify(response))
-	}
+	}	
 })
 
+
+// Start the server on the specified port
 var server = app.listen(port, function () {
 	console.log("Middleware listening at port %s", port)
 })
 
-function parseTime(time) {
-	var result = "0000";
-	var front = parseInt(time.slice(0,2))
-	var back = parseInt(time.slice(2,4))
 
-	if(back < 30) {
-		if(front < 10) {
-			result = "0" + front.toString() + "00"
-		} else {
-			result = front.toString() + "00"
-		}
-	} else {
-		if(front+1 < 10) {
-			result = "0" + (front+1).toString() + "00"
-		} else if(front+1 <= 24) {
-			result = (front+1).toString() + "00"
-		}
-	}
-	
-	return result
-}
-
-function parseDay(day) {
-	if(!(day in forecastJson)) {
-		return "mon"
-	} else {
-		return day
-	}
-}
-
+// Function to recolor the SVG based on the provided time and day
 function recolorSVG(time, day) {
 	var file = fs.readFileSync(__dirname + "/" + "lot.svg", 'utf8')
 	if (!file) {
